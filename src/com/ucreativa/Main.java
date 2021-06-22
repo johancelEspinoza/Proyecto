b9e9a20195b13748b3c7c584ec94b1644d5cd029
johancelEspinoza <84363312+johancelEspinoza@users.noreply.github.com>
2021-06-21 19:14:16 -0600

    Final

---
+++ src/com/ucreativa/Main.java
@@ -0,0 +1,13 @@
+package com.ucreativa;
+
+import com.ucreativa.ui.FrontEnd;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FrontEnd fe = new FrontEnd("Reporte de Ingreso de Vehiculos");
+        fe.build();
+
+    }
+}//end
